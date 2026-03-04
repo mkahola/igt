4aff2e43cb4a07d16e9b203d77705e29fa50cb9e
Sasha McIntosh <sashamcintosh@google.com>
2026-03-04 13:46:35 -0500

    include/android: Define additional types in glib.h
    Define additional types to satisfy dependencies in kms_colorop.c to fix
    breakages in the Android build environment.
    
    Test: kms_colorop
    
    Signed-off-by: Sasha McIntosh <sashamcintosh@google.com>
    Acked-by: Kamil Konieczny <kamil.konieczny@linux.intel.com>

--- include/android/glib.h
+++ include/android/glib.h
@@ -8,6 +8,7 @@
 
 #define G_KEY_FILE_NONE 0
 #define G_REGEX_OPTIMIZE 0
+#define GINT_TO_POINTER(i) ((void *)(signed long)(i))
 
 typedef struct _GError {
 	int code;
@@ -23,6 +24,11 @@ typedef struct _GMatchInfo {
 	int dummy;
 } GMatchInfo;
 
+typedef struct _GHashTable {
+  unsigned int hash_func;
+  bool key_equal_func;
+} GHashTable;
+
 typedef int gint;
 typedef size_t gsize;
 typedef char gchar;
@@ -30,6 +36,9 @@ typedef unsigned char guchar;
 typedef void GRegex;
 typedef int GRegexMatchFlags;
 typedef bool gboolean;
+typedef unsigned int guint;
+typedef guint (*GHashFunc)(const void *key);
+typedef gboolean (*GEqualFunc)(const void *a, const void *b);
 
 static inline void g_clear_error(GError **error) { }
 static inline void g_error_free(GError *error) { }
@@ -52,5 +61,18 @@ static inline gboolean g_regex_match(GRegex *regex, const gchar *string,
 	GRegexMatchFlags match_options, GMatchInfo **match_info) { return false; }
 
 static gchar *g_base64_encode(const guchar *data, gsize len) { return NULL; }
+static guint g_direct_hash(const void *v) { return 0; }
+static gboolean g_direct_equal(const void *v1, const void *v2) { return 0; }
+static inline GHashTable *g_hash_table_new(GHashFunc hash_func,
+                                           GEqualFunc key_equal_func) {
+  return NULL;
+}
+static inline void g_hash_table_destroy(GHashTable *hash_table) { }
+static inline gboolean g_hash_table_add(GHashTable *hash_table, void *key) {
+  return false;
+}
+static inline gboolean g_hash_table_contains(GHashTable *hash_table, const void *key) {
+  return false;
+}
 
 #endif /* _IGT_ANDROID_GLIB_H */
