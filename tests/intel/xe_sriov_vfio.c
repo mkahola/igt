2f287265245ef641e55086ebc625e704f48a391f
Marcin Bernatowicz <marcin.bernatowicz@linux.intel.com>
2026-03-27 15:06:25 +0100

    tests/intel/xe_sriov_vfio: Add dynamic bind-unbind-vfs subtest
    For each supported numvfs-N configuration, enable N VFs and
    bind/unbind each VF to xe-vfio-pci through driver_override.
    
    Signed-off-by: Marcin Bernatowicz <marcin.bernatowicz@linux.intel.com>
    Cc: Adam Miszczak <adam.miszczak@linux.intel.com>
    Cc: Jakub Kolakowski <jakub1.kolakowski@intel.com>
    Cc: Kamil Konieczny <kamil.konieczny@linux.intel.com>
    Cc: Lukasz Laguna <lukasz.laguna@intel.com>
    Reviewed-by: Lukasz Laguna <lukasz.laguna@intel.com>

--- tests/intel/xe_sriov_vfio.c
+++ tests/intel/xe_sriov_vfio.c
@@ -3,8 +3,11 @@
  * Copyright(c) 2026 Intel Corporation. All rights reserved.
  */
 
+#include "drmtest.h"
 #include "igt_core.h"
 #include "igt_kmod.h"
+#include "igt_pci.h"
+#include "igt_sriov_device.h"
 
 /**
  * TEST: xe_sriov_vfio
@@ -16,13 +19,134 @@
  *
  * SUBTEST: load-unload-xe-vfio-pci
  * Description: Attempt to load xe_vfio_pci module and then unload it.
+ *
+ * SUBTEST: bind-unbind-vfs
+ * Description: Enable VFs and bind/unbind each one to xe-vfio-pci via driver_override.
  */
 
 IGT_TEST_DESCRIPTION("Xe SR-IOV VFIO tests (xe-vfio-pci)");
 
-static const char *XE_VFIO_PCI_MOD = "xe_vfio_pci";
+#define DRIVER_OVERRIDE_TIMEOUT_MS 200
 
+static const char *XE_VFIO_PCI_MOD = "xe_vfio_pci";
+static const char *XE_VFIO_PCI_DRV = "xe-vfio-pci";
+
+static int pf_fd = -1;
+static bool autoprobe;
 static bool xe_vfio_loaded_initially;
+
+static void assert_xe_vfio_pci_is_bound(const char *pci_slot)
+{
+	char bound[64];
+	int ret;
+
+	ret = igt_pci_get_bound_driver_name(pci_slot, bound, sizeof(bound));
+	igt_assert_f(ret >= 0, "Failed to read bound driver for %s (%d)\n", pci_slot, ret);
+	igt_assert_f(ret > 0, "Expected %s to be bound to %s, but it is unbound\n",
+		     pci_slot, XE_VFIO_PCI_DRV);
+	igt_assert_f(!strcmp(bound, XE_VFIO_PCI_DRV),
+		     "Expected %s to be bound to %s, got %s\n",
+		     pci_slot, XE_VFIO_PCI_DRV, bound);
+}
+
+static void assert_driver_unbound(const char *pci_slot)
+{
+	char bound[64];
+	int ret;
+
+	ret = igt_pci_get_bound_driver_name(pci_slot, bound, sizeof(bound));
+	igt_assert_f(ret >= 0, "Failed to read bound driver for %s (%d)\n", pci_slot, ret);
+	igt_assert_f(ret == 0, "Expected %s to be unbound, but is bound to %s\n",
+		     pci_slot, bound);
+}
+
+static char *vf_pci_slot_alloc(unsigned int vf_id)
+{
+	char *slot = igt_sriov_get_vf_pci_slot_alloc(pf_fd, vf_id);
+
+	igt_assert_f(slot, "Failed to get VF%u PCI slot\n", vf_id);
+	return slot;
+}
+
+static void vf_bind_override(unsigned int vf_id, const char *driver)
+{
+	char *slot = vf_pci_slot_alloc(vf_id);
+	int ret;
+
+	ret = igt_pci_bind_driver_override(slot, driver, DRIVER_OVERRIDE_TIMEOUT_MS);
+	igt_assert_f(ret == 0, "bind %s (VF%u) to %s failed (%d)\n", slot, vf_id, driver, ret);
+	assert_xe_vfio_pci_is_bound(slot);
+
+	free(slot);
+}
+
+static void vf_unbind_override(unsigned int vf_id)
+{
+	char *slot = vf_pci_slot_alloc(vf_id);
+	int ret;
+
+	ret = igt_pci_unbind_driver_override(slot, DRIVER_OVERRIDE_TIMEOUT_MS);
+	igt_assert_f(ret == 0, "unbind %s (VF%u) failed (%d)\n", slot, vf_id, ret);
+	assert_driver_unbound(slot);
+
+	free(slot);
+}
+
+static void bind_unbind_vfs(unsigned int num_vfs)
+{
+	igt_sriov_disable_driver_autoprobe(pf_fd);
+	igt_sriov_enable_vfs(pf_fd, num_vfs);
+
+	igt_require_f(!igt_pci_system_reinit(), "Failed to refresh PCI state\n");
+
+	igt_sriov_enable_driver_autoprobe(pf_fd);
+
+	for (unsigned int vf_id = 1; vf_id <= num_vfs; vf_id++) {
+		vf_bind_override(vf_id, XE_VFIO_PCI_DRV);
+		vf_unbind_override(vf_id);
+	}
+
+	igt_sriov_disable_vfs(pf_fd);
+}
+
+static void open_pf(void)
+{
+	int fd;
+
+	if (pf_fd >= 0)
+		return;
+
+	fd = drm_open_driver(DRIVER_XE);
+	igt_assert_fd(fd);
+
+	if (!igt_sriov_is_pf(fd)) {
+		drm_close_driver(fd);
+		igt_skip("Xe device is not an SR-IOV PF\n");
+	}
+
+	if (igt_sriov_get_enabled_vfs(fd) != 0) {
+		drm_close_driver(fd);
+		igt_skip("VFs must be disabled before running this test\n");
+	}
+
+	autoprobe = igt_sriov_is_driver_autoprobe_enabled(fd);
+	pf_fd = fd;
+}
+
+static void cleanup_pf(void)
+{
+	if (pf_fd < 0)
+		return;
+
+	igt_sriov_disable_vfs(pf_fd);
+	igt_abort_on_f(igt_sriov_get_enabled_vfs(pf_fd) > 0, "Failed to disable VF(s)\n");
+	autoprobe ? igt_sriov_enable_driver_autoprobe(pf_fd) :
+		   igt_sriov_disable_driver_autoprobe(pf_fd);
+	igt_abort_on_f(autoprobe != igt_sriov_is_driver_autoprobe_enabled(pf_fd),
+		       "Failed to restore sriov_drivers_autoprobe value\n");
+	drm_close_driver(pf_fd);
+	pf_fd = -1;
+}
 
 static void restore_xe_vfio_module(void)
 {
@@ -69,7 +193,22 @@ int igt_main()
 		igt_assert(igt_kmod_is_loaded(XE_VFIO_PCI_MOD) == false);
 	}
 
+	igt_describe("Enable VFs and bind/unbind each one to xe-vfio-pci via driver_override.");
+	igt_subtest_with_dynamic("bind-unbind-vfs") {
+		igt_skip_on_f(igt_kmod_load(XE_VFIO_PCI_MOD, NULL),
+			      "Failed to load %s\n", XE_VFIO_PCI_MOD);
+
+		open_pf();
+
+		for_each_sriov_num_vfs(pf_fd, num_vfs) {
+			igt_dynamic_f("numvfs-%u", num_vfs) {
+				bind_unbind_vfs(num_vfs);
+			}
+		}
+	}
+
 	igt_fixture() {
+		cleanup_pf();
 		restore_xe_vfio_module();
 	}
 }
