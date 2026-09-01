cc7accdb115feaf657e382db33f415fc9d3a34ea
Jani Nikula <jani.nikula@intel.com>
2026-09-01 11:46:37 +0300

    tests/kms_chamelium_color_pipeline: prefer igt_crtc_name() over kmstest_pipe_name()
    Reduce the direct pipe usage.
    
    Reviewed-by: Kamil Konieczny <kamil.konieczny@linux.intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- tests/chamelium/kms_chamelium_color_pipeline.c
+++ tests/chamelium/kms_chamelium_color_pipeline.c
@@ -188,7 +188,7 @@ test_plane_colorops(data_t *data, igt_crtc_t *crtc,
 		if (!igt_plane_has_prop(plane, IGT_PLANE_COLOR_PIPELINE))
 			continue;
 
-		igt_dynamic_f("pipe-%s-plane-%u", kmstest_pipe_name(crtc->pipe), plane_id)
+		igt_dynamic_f("pipe-%s-plane-%u", igt_crtc_name(crtc), plane_id)
 			_test_plane_colorops(data, plane, fb_colors,
 					     exp_colors, colorops,
 					     data->ports[port_idx]);
